c230e913139a2f3d81acc471de1f262182190513
Lucas Áquila <lucaszaquila@gmail.com>
2022-06-22 14:22:21 -0300

    Uploading project

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,44 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    avaliableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+    const isLessonAvailable = true;
+
+    return (
+        <a href="">
+            <span className="text-gray-300">
+                {props.avaliableAt.toString()}
+            </span>
+
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header className="flex items-center justify-between">
+                    {isLessonAvailable ? (
+                        <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+                            <CheckCircle size={20} />
+                            Conteúdo Liberado
+                        </span>
+                    ) : (
+                        <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+                    )}
+                    <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
+                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+                    </span>
+                </header>
+
+                <strong className="text-grey-200 mt-5 block">
+                    {props.title}
+                </strong>
+            </div>
+        </a>
+    )
+}
